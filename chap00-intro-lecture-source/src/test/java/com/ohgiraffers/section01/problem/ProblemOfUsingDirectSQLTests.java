01884dfca4e3f25d52b00152ccd824a75b4623c4
orlzll <>
2024-02-26 09:50:51 +0900

    config

---
+++ chap00-intro-lecture-source/src/test/java/com/ohgiraffers/section01/problem/ProblemOfUsingDirectSQLTests.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.section01.problem;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ProblemOfUsingDirectSQLTests {
+
+    private Connection con;
+
+    @BeforeEach
+    void setConnection() throws ClassNotFoundException, SQLException {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/menudb";
+        String user = "swcamp";
+        String password = "swcamp";
+
+        Class.forName(driver);
+
+        con = DriverManager.getConnection(url, user, password);
+        con.setAutoCommit(false);
+
+    }
+}
